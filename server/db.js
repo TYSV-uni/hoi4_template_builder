843890bb58340bc05f81ad6f2cb4d3827cdf5b2b
TYSV-uni <sventsytskyi.tymofii@lll.kpi.ua>
2026-05-17 10:08:54 +0300

    deployment for railway

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host:     process.env.PGHOST,
-    port:     process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user:     process.env.PGUSER,
-    password: process.env.PGPASSWORD,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 async function initDB() {
