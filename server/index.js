fea9d91f8df9e79b61b663db107f35a98cbe044f
TYSV-uni <sventsytskyi.tymofii@lll.kpi.ua>
2026-05-17 09:48:08 +0300

    deployment for railway

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const { pool, initDB } = require('./db');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
